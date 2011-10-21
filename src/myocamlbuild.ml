8f1b4f5753418294e0dff215576cb1e908939a49
darioteixeira <darioteixeira@yahoo.com>
2011-10-21 14:31:12 +0000

    Added myocamlbuild.ml

---
+++ src/myocamlbuild.ml
@@ -0,0 +1,20 @@
+(********************************************************************************)
+(* Ocamlbuild plugin for Litiom project.					*)
+(********************************************************************************)
+
+open Ocamlbuild_plugin
+
+
+let _ = dispatch begin function
+
+	| After_rules ->
+
+		flag ["ocaml"; "pack"] (S[A"-I"; A"lib"]);
+
+		flag ["ocaml"; "use_thread"; "compile"] (S[A "-thread"]);
+		flag ["ocaml"; "use_thread"; "link"] (S[A "-thread"]);
+		flag ["ocaml"; "use_thread"; "infer_interface"] (S[A "-thread"])
+
+	| _ -> ()
+end
+
